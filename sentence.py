217798b036d10549266bd5ddc43fdb5cf9f8eeea
Thy Ton <thynt@uci.edu>
2019-07-09 02:55:02 +0000

    Added Sentence class and test

--- sentence.py
+++ sentence.py
@@ -6,26 +6,94 @@ import numpy as np
 import copy 
 
 class AtomicSentence:
-	" ""A truth table of a proposition"" "
+	"""An atomic sentence consists of 
+		- its symbol 
+		- whether it is a positive literal
+		- its true table
+	For example: p is a positive literal -> positive == true
+				 ¬p is a negative literal -> positive == false
+	"""
 
-	def __init__(self, val = None):	
+	def __init__(self, positive = None, symbol = None):
 		try:
-			val
-			self.v = val
+			assert(isinstance(positive, bool))
+			self._p = positive
 		except:
-			self.v = True
+			print("'positive' is not a bool type.")
 
-		self.t = np.array([True, False])
+		try:
+			assert(symbol is not None and isinstance(symbol, str))
+			self._s = symbol
+			if self._s == "True":
+				self._p = True
+				self._t = np.array(True)
+			elif self._s == "False":
+				self._p = True
+				self._t = np.array(False)
+			else:
+				self._t = np.array([True, False])
+		except:
+			print("No string symbol for the proposition is given")
 
 	def table(self):
-		return self.t
+		return self._t
 
 	def set(self, val):
-		return AtomicSentence(val)
+		cp = copy.deepcopy(self)
+		cp._t = np.array(val)
+		return cp
 
-	def val(self):
-		return self.v
+	# magic	
+	def __str__(self):
+		# neg = bytes.decode(172)
+		neg = "-"
+		return self._s if self._p else neg + self._s
+
+	def __not__(self):
+		cp = Sentence(cp)
+		cp = not cp
+		return cp
+
+	# fake method to test if an object is of AtomicSentence
+	def atom(self):
+		return 
+
+import pyPL.connective
+
+class Sentence(AtomicSentence):
+	pass
+	def __init__(self, obj):
+		try:
+			obj.atom()
+			self._p = obj._p	# is positive
+			self._s = obj._s   # symbol
+			self._t = obj._t   # truth table
+
+			try:
+				obj.complex()
+				self._pns = obj._pns	# a queue that represents postfix notation string of of the complex sentence
+			except:
+				self._pns = [obj._pns]
+		except:
+			print("Argument 'obj' is not an instance of AtomicSentence")
+	
+	# magic - some logical connectives
 
 
-# class Sentence:
-# 	pass
+	# overriden __not__ from the base class
+	def __not__():
+		self = not self._p
+		self._pns += Conn.NOT
+		print(self._pns)
+
+	# fake method to test if an objec is of Sentence
+	def complex(self):
+		return
+
+
+
+
+
+
+
+
--- tests/testSentence.py
+++ tests/testSentence.py
@@ -1,15 +1,30 @@
 import sys
+
 sys.path.append('../../')
 from pyPL.sentence import AtomicSentence
+from pyPL.sentence import Sentence
+
 import unittest
 import numpy as np 
 
-
-class testSentence(unittest.TestCase):
+class testAtomicSentence(unittest.TestCase):
 	def test_table(self):
-		print(AtomicSentence([True,False]).table())
+		print(AtomicSentence(True, "p").table())
+
 	def test_set(self):
-		print(AtomicSentence().set(False).val())
+		print(AtomicSentence(True, "True").set(False).table())
+
+	def test_str(self):
+		print(AtomicSentence(False, "p"))
+		print(AtomicSentence(True, "p"))
+
+	def test_neg(self):
+		s = AtomicSentence(True, "p")
+		s = -s
+		print(s)
+
+	def test_not(self):
+		s = AtomicSentence(True, "p")
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity = 2)
